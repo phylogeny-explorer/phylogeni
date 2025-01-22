183e3c75a9517fc171b1b0afd932564289546676
ch1ru <caleb1@criptext.com>
2025-01-22 01:31:47 +0000

    feat(pages): add styling and frontend validation to account page

--- src/app/(user)/account/account-form.tsx
+++ src/app/(user)/account/account-form.tsx
@@ -1,17 +1,28 @@
 'use client';
 
-import { Input, Stack } from '@chakra-ui/react';
+import {
+  Box,
+  FileUploadTrigger,
+  Heading,
+  Input,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import type { User } from '@supabase/supabase-js';
-import { useState } from 'react';
-
+import { useRef, useState } from 'react';
+import { toaster, Toaster } from '~/components/ui/toaster';
 import { Avatar } from '~/components/ui/avatar';
 import { Button } from '~/components/ui/button';
 import { Field } from '~/components/ui/field';
 import { createClient } from '~/lib/utils/supabase/client';
+import { LuLogOut } from 'react-icons/lu';
+import { FileUploadRoot } from '~/components/ui/file-upload';
+import { HiUpload } from 'react-icons/hi';
 
 interface Props extends User {
   full_name?: string;
   avatar_url?: string;
+  updated_at?: string;
 }
 
 export default function AccountForm({
@@ -19,55 +30,203 @@ export default function AccountForm({
   email,
   full_name,
   avatar_url,
+  updated_at,
 }: Props) {
   const supabase = createClient();
   const [loading, setLoading] = useState(false);
   const [fullname, setFullname] = useState(full_name);
+  const [lastUpdatedAt, setLastUpdatedAt] = useState(updated_at);
+
+  const avatarRef = useRef<HTMLInputElement>(null);
 
   const updateProfile = async () => {
     try {
       setLoading(true);
+      const newUpdatedAt = new Date().toISOString();
+
+      const files = avatarRef.current?.files;
+      if (files) {
+        console.log(files[0]);
+      }
 
       const { error } = await supabase.from('profiles').upsert({
         id: id as string,
         full_name: fullname,
-        updated_at: new Date().toISOString(),
+        updated_at: newUpdatedAt,
       });
+
       if (error) throw new Error(error.message);
-      alert('Profile updated!');
+
+      //Updates the state without needing to refresh
+      setLastUpdatedAt(newUpdatedAt);
+      setFullname(fullname);
+
+      toaster.create({
+        title: 'Profile updated',
+        description: `Last updated ${updated_at}`,
+        //type: "success",
+      });
     } catch (error) {
-      alert('Error updating the data!');
+      console.log(error);
+      toaster.create({
+        title: 'Something went wrong',
+        description: 'Profile was not updated',
+        type: 'error',
+      });
     } finally {
       setLoading(false);
     }
   };
 
+  const validateString = (val: string) => {
+    //checks if string contains numbers or special chars
+    const matches = val.match(/\d|[$&\\+,:;=?@#|'<>.^*()%!-]/g);
+    if (matches !== null) return false;
+    return true;
+  };
+
   return (
-    <Stack as="main" margin={8} marginY={22}>
-      {avatar_url && <Avatar size="xl" src={avatar_url} name={full_name} />}
-      <Field label="Email">
-        <Input id="email" type="text" value={email} disabled />
-      </Field>
-      <Field label="Full name">
-        <Input
-          id="fullName"
-          type="text"
-          value={fullname || ''}
-          onChange={(e) => setFullname(e.target.value)}
-        />
-      </Field>
+    <Stack
+      as="main"
+      width={{ xl: '50%', mdDown: '90%' }}
+      margin={'auto'}
+      padding={{ base: '20px', md: '40px' }}
+      marginTop={{ xl: '6rem', md: 0 }}
+    >
+      <Heading
+        fontSize={'2rem'}
+        justifyContent={'center'}
+        alignContent={'center'}
+        width={'full'}
+        textAlign={'center'}
+      >
+        Account Details
+      </Heading>
 
-      <div>
-        <Button onClick={updateProfile} disabled={loading}>
-          {loading ? 'Loading ...' : 'Update'}
-        </Button>
-      </div>
+      <Box
+        display={'flex'}
+        flexDir={{ xlTo2xl: 'row', mdDown: 'column' }}
+        marginTop={'3rem'}
+        gap={'3rem'}
+        justifyContent={'space-between'}
+      >
+        <Box
+          width={'100%'}
+          display={'flex'}
+          flexDirection={'column'}
+          justifyContent={'space-between'}
+        >
+          <Field label="Email">
+            <Input id="email" type="text" value={email} disabled />
+          </Field>
+          <Field
+            invalid={!fullname || !validateString(fullname)}
+            label="Full name"
+            errorText="Invalid"
+            style={{ marginTop: '2rem' }}
+          >
+            <Input
+              id="fullName"
+              type="text"
+              value={fullname || ''}
+              onChange={(e) => setFullname(e.target.value)}
+            />
+          </Field>
 
-      <div>
-        <form action="/api/auth/signout" method="post">
-          <Button type="submit">Sign out</Button>
-        </form>
-      </div>
+          <Text
+            color={'grey'}
+            marginTop={'2rem'}
+            paddingBottom={'.4rem'}
+            fontWeight={'light'}
+            fontSize={'.9rem'}
+          >
+            Last updated{' '}
+            {lastUpdatedAt && new Date(lastUpdatedAt).toDateString()}
+          </Text>
+        </Box>
+
+        <Box
+          width={'100%'}
+          display={'flex'}
+          flexDirection={'column'}
+          justifyContent={'space-between'}
+        >
+          <Box display={'flex'} justifyContent={'center'}>
+            {avatar_url ? (
+              <Avatar
+                size="xl"
+                src={avatar_url}
+                name={full_name}
+                width={'9rem'}
+                height={'9rem'}
+              />
+            ) : (
+              <Avatar
+                background={'var(--chakra-colors-teal-800)'}
+                color={'var(--chakra-colors-teal-400)'}
+                width={'9rem'}
+                height={'9rem'}
+                name={full_name}
+              />
+            )}
+          </Box>
+          <Box
+            display={'flex'}
+            gap={'15px'}
+            alignItems={'center'}
+            alignContent={'center'}
+            marginTop={'2rem'}
+            justifyContent={'center'}
+          >
+            <Button
+              _hover={{ color: '#860111' }}
+              background={'none'}
+              color={'#c23b22'}
+            >
+              Remove avatar
+            </Button>
+
+            <Box width={'max-content'}>
+              <FileUploadRoot accept={'image/*'} ref={avatarRef}>
+                <FileUploadTrigger asChild>
+                  <Button id="AvatarFileUpload" variant="outline" size="sm">
+                    <HiUpload /> Upload file
+                  </Button>
+                </FileUploadTrigger>
+              </FileUploadRoot>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box
+        display={'flex'}
+        justifyContent={'space-between'}
+        alignContent={'center'}
+        marginTop={'6rem'}
+      >
+        <Box>
+          <form action="/api/auth/signout" method="post">
+            <Button
+              _hover={{ color: 'teal' }}
+              background={'none'}
+              fontWeight={'light'}
+              textAlign={'left'}
+              padding={0}
+              type="submit"
+            >
+              <LuLogOut /> Sign out
+            </Button>
+          </form>
+        </Box>
+
+        <Box>
+          <Button type={'submit'} onClick={updateProfile} disabled={loading}>
+            {loading ? 'Loading ...' : 'Update'}
+          </Button>
+        </Box>
+      </Box>
+      <Toaster />
     </Stack>
   );
 }
